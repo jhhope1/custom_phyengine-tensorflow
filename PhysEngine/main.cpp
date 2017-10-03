0142fca2992b9b8f2a912238aab6e1240999dfcd
sshskjh <jhhope1@naver.com>
2017-10-03 10:27:38 +0900

    컴파일 안되는 개구데기 막대

--- PhysEngine/main.cpp
+++ PhysEngine/main.cpp
@@ -1,6 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 using phys = double;
+
+//for Cylinder
+const double M=10;
+const double h=0.5;
+const double R=1;
+const double g=9.81;
+
+
 struct Vector{
     phys x,y,z;
     Vector(phys x=0.L,phys y=0.L, phys z=0.L):x(x),y(y),z(z){}
@@ -80,12 +88,75 @@ struct Mat33{
                       mat[2][0]*z.x+mat[2][1]*z.y+mat[2][2]*z.z);
     }
 };
-struct Rigidbody2d{
+struct Rigidbody{
     phys m,speed;
-    Mat33 I,invI;
+    Mat33 I, invI;
     Vector r,v,vb,w,euler,F,tau;
     Quat qori;
 };
-int main(){
 
+void flyingstick(){
+    vector <Rigidbody> stick(1);
+
+    stick[0].I.mat[0][0]=double(1/12)*M*h*h+1/4*M*R*R;
+    stick[0].I.mat[1][1]=double(1/12)*M*h*h+1/4*M*R*R;
+    stick[0].I.mat[2][2]=double(1/2)*M*R*R;
+    for(int i=0 ; i<3 ; i++){
+        for(int j=0 ; j<3 ; j++){
+            printf("%.3f\t", stick[0].I.mat[i][j]);
+        }
+        printf("\n");
+    }
+    stick[0].tau=Vector(0,0,0)
+    stick[0].r=Vector(0,0,0);
+    phys eux=stick[0].euler.x;
+    phys euy=stick[0].euler.y;
+    phys euz=stick[0].euler.z;
+    //이 밑은 계산 훨씬 줄일수 있음. 계산하면 됨. 추후에 바꿔야할 것.
+    Mat33 RollMat;
+    RollMat.mat[0][0]=1;
+    RollMat.mat[1][1]=cos(eux);
+    RollMat.mat[1][2]=sin(eux);
+    RollMat.mat[2][1]=-sin(eux);
+    RollMat.mat[2][2]=cos(eux);
+
+    Mat33 PitchMat;
+    PitchMat.mat[0][0]=cos(euy);
+    PitchMat.mat[0][2]=-sin(euy);
+    PitchMat.mat[1][1]=1;
+    PitchMat.mat[2][0]=sin(euy);
+    PitchMat.mat[2][2]=cos(euy);
+
+    Mat33 YawMat;
+    YawMat.mat[0][0]=cos(euz);
+    YawMat.mat[0][1]=sin(euz);
+    YawMat.mat[1][0]=-sin(euz);
+    YawMat.mat[1][1]=cos(eux);
+    YawMat.mat[2][2]=1;
+
+    Mat33 Rotatemat;
+    Rotatemat.mat=RollMat.mat*(PitchMat.mat*YawMat.mat);//문법은 몰라요~~초기화는 알아서 빼서 하세요~~
+
+    Vector wprime ;
+    wprime= Rotatemat.mat*stick[0].w;//초기화는 알아서~
+    Mat33 WprimeMat;
+
+    WprimeMat.mat={0,-wprime.z, wprime.y, wprime.z, 0, -wprime.z, -wprime.y, wprime.x, 0};//문법은 몰라요~~초기화는 알아서 빼서 하세요~~
+
+    Vector alphaprime;
+    alphaprime=stick[0].invI*(stick[0].tau-(WprimeMat.mat*stick[0].I+stick[0].I*WprimeMat.inv()*stick[0].w);//또 초기화는 알아서~
+
+    Vector alpha;
+    alpha=Rotatemat.inv()*alphaprime.Vector();
+    stick[0].w+=alpha.Vector()*dtime;//스칼라곱 정의해놓을것
+    stick[0].euler+=stick[0].w*dtime;//마찬가지
+
+    Vector a;
+    a=Vector(0,0,0);
+    stick[0].v+=a*dtime;//스칼라곱 정의해놓을것
+    stick[0].r+=stick[0].v*dtime;
+
+}
+int main(){
+    flyingstick();
 }
