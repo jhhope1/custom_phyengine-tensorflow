fed7351327eb0a64d5558b8e9d7ee4a66231a905
sshskjh <jhhope1@naver.com>
2017-10-14 22:45:51 +0900

    믕.. 일단 물리엔진은 돌아갈듯 하다.ploting을 추가 해야될듯

--- phyengine with tf_171014/phyengine with tf/phyengine_with_tf.py
+++ phyengine with tf_171014/phyengine with tf/phyengine_with_tf.py
@@ -3,7 +3,7 @@ import numpy as np
 
 numsubleg = 3
 numLeg = 4
-Mtot = 0.5
+Mtot = 0.9849
 dtime = 0.01
 Fupscale = 3.
 Fdownscale = 1.
@@ -16,6 +16,7 @@ Fsubed = tf.constant([[0,0,Mtot*(Fupscale-Fdownscale)*9.81]],dtype=tf.float64)
 Offset = tf.constant([[0,0,0.5]],dtype=tf.float64)
 MxMyFilter = tf.constant([[-1.,-1.,0]],dtype=tf.float64)
 global_step = tf.Variable(0,trainable = False, name = 'global_step')
+
 
 Destination = tf.placeholder(tf.float64, [None,3])
 rs_deprec = tf.placeholder(tf.float64, [None,3])
@@ -123,8 +124,9 @@ class robot:
                                                  [0.,1.40e-5,0.],
                                                  [0.,0.,2.20e-5]],dtype=tf.float64)
             #set Initial theta conditions
-
-            self.leg[p].sub[1].theta = np.pi / 2.
+            for i in range(numsubleg):
+                self.leg[p].sub[i].theta = tf.constant(0., dtype=tf.float64)
+            self.leg[p].sub[1].theta = tf.constant(np.pi / 2., dtype=tf.float64)
 
         self.body.m = (0.2676 + 0.1414) \
                       +self.leg[0].sub[0].m \
@@ -132,11 +134,13 @@ class robot:
                       -self.leg[0].sub[2].m \
                       +(0.1583 + 0.2358)
 
+        Mtot=self.body.m + 4 * (self.leg[0].sub[0].m+self.leg[0].sub[1].m+self.leg[0].sub[2].m)
         self.body.Ib = tf.constant([[75.0e-5,0.,0.],
                                     [0.,75.0e-5,0.],
                                     [0.,0.,50.0e-5]],dtype=tf.float64)
 
-        self.body.rs += tf.constant([0,0,0.25],dtype=tf.float64)
+        self.body.rs = tf.constant([0,0,0.3],dtype=tf.float64)
+        self.body.Q=tf.constant([[1,0,0],[0,1,0],[0,0,1]], dtype=tf.float64)
 
         
     def setalpha(self,t = 0.0):
@@ -144,7 +148,6 @@ class robot:
     def timeflow(self,t = 0.0):
         self.setalpha(t)
         Feqc = tf.scalar_mul(Mtot,g)
-        #print("Feqc = mg = ",Feqc)
         Feqa = tf.diag([Mtot,Mtot,Mtot])
         Crossvec = tf.zeros((1,3),dtype=tf.float64)
         Teqalpha = tf.zeros((3,3),dtype=tf.float64)
@@ -152,8 +155,6 @@ class robot:
         mlsum = tf.zeros((1,3),dtype=tf.float64)
         sumDs = tf.zeros((3,3),dtype=tf.float64)
         wbs = tf.matmul(self.body.wb, self.body.Q) #[1,3] matrix
-        print(wbs)
-        print("sssss")
 
         for p in range(numLeg):
            Qs = [tf.matmul(self.leg[p].sub[0].Q , self.body.Q)]
@@ -188,24 +189,19 @@ class robot:
 
            lbtomotbs = tf.matmul(self.body.lbtomot[p] , self.body.Q) # lbtomotbs = 2Dtensor
            lbtomots = [ lbtomotbs + ls[0][0] ] # lbtomots = 2Dtensor
-
            for i in range(1,numsubleg):
                lbtomots.append(lbtomots[i-1]+ls[i-1][1]+ls[i][0])
-           #print(mlsum)
            for i in range(numsubleg):
                mlsum += tf.scalar_mul(self.leg[p].sub[i].m, lbtomots[i])
-           #print(lbtomots[0])
-           #print("after adding lbtomots : ",mlsum)
 
            #Calculating External Forces
            for i in range(numsubleg):
                Collision = tf.cast(tf.less(lbtomots[i]-self.body.rs,tf.zeros((1,3),dtype=tf.float64)),tf.float64)
                vs = tf.cross(ws[i], lbtomots[i])
                vCollision = tf.cast(tf.less( vs , tf.zeros((1,3),dtype=tf.float64) ),tf.float64)
-               Ftemp = tf.multiply(Collision, Fadded + tf.multiply( (vCollision - Offset) , Fsubed ))
+               Ftemp = tf.multiply((tf.ones((1,3), tf.float64)-Collision), Fadded + tf.multiply( (vCollision - Offset) , Fsubed ))
                Feqc += Ftemp
                Teqc += tf.cross( lbtomots[i]-self.body.rs , Ftemp )
-               print(lbtomots[0])
                FrictionTemp = tf.scalar_mul( Fricscale , tf.multiply( MxMyFilter, vs ) )
                Feqc += FrictionTemp
                Teqc += tf.cross( lbtomots[i]-self.body.rs , FrictionTemp )
@@ -231,23 +227,16 @@ class robot:
                Dya = tf.scalar_mul(self.leg[p].sub[i].m,tf.matmul(lbtomots[i],lbtomots[i],transpose_a=True))
            
            Ds = tf.diag(tf.concat([mlsquare,mlsquare,mlsquare], axis=0)) - Dya
-           #print(Teqalpha)
            Teqalpha += Ds 
            sumDs += Ds
-           #print(Teqalpha)
            #Qb * Ib * Qb.transpose()
 
 
-
-           #print(Ds)
-           #print("QIw^2 = ",tf.matmul( Qs[i], tf.cross( tf.matmul(self.leg[p].sub[i].Ib, w[i]) , w[i] )))
-           #print("IQa = ",tf.matmul( Is[i], Qalphasum[i] ))
            for i in range(numsubleg):
                Feqc -= tf.scalar_mul(self.leg[p].sub[i].m,A[i])
                Crossvec += tf.scalar_mul(self.leg[p].sub[i].m,lbtomots[i])
                Teqc += tf.matmul( tf.cross( tf.matmul(w[i] , self.leg[p].sub[i].Ib) , w[i] ), Qs[i])
                Teqc -= tf.matmul( Qalphasum[i] , Is[i])
-               print("cross = ", tf.cross( tf.matmul(w[i] , self.leg[p].sub[i].Ib) , w[i] ))
                Teqalpha += Is[i]
                #Qs_i * I_i * Qs_i^T
 
@@ -256,57 +245,33 @@ class robot:
            for i in range(numsubleg):
                self.leg[p].sub[i].omega += self.leg[p].sub[i].alpha * dtime
                self.leg[p].sub[i].theta += self.leg[p].sub[i].omega * dtime
-               self.leg[p].sub[i].Q = tf.scalar_mul(tf.cos(self.leg[p].sub[i].theta), tf.eye(3),dtype=tf.float64) + \
-               tf.scalar_mul(1.-tf.cos(self.leg[p].sub[i].theta), tf.matmul(self.leg[p].sub[i].axis,self.leg[p].sub[i].axis,transpose_a = True),dtype=tf.float64) + \
+               self.leg[p].sub[i].Q = tf.scalar_mul(tf.cos(self.leg[p].sub[i].theta), tf.eye(3, dtype=tf.float64)) + \
+               tf.scalar_mul(1.-tf.cos(self.leg[p].sub[i].theta), tf.matmul(self.leg[p].sub[i].axis,self.leg[p].sub[i].axis,transpose_a = True)) + \
                tf.scalar_mul(tf.cos(self.leg[p].sub[i].theta) , tf.cross(tf.concat([self.leg[p].sub[i].axis,
                                                                                     self.leg[p].sub[i].axis,
                                                                                     self.leg[p].sub[i].axis],axis=0)
-                                                                                   ,tf.eye(3)),dtype=tf.float64)
-               print(tf.concat([self.leg[p].sub[i].axis,self.leg[p].sub[i].axis,self.leg[p].sub[i].axis],axis=0))
-               #print("Qmat = ",self.leg[p].sub[i].Q)
+                                                                                   ,tf.eye(3, dtype=tf.float64)))
                #update 'Q's of leg - 20171012 fine
-        #print("Teqc out loop",Teqc);
-        print("asdfasdf",self.body.wb)
-        #print("out of loop",Teqalpha)
         Teqalpha += tf.matmul( tf.matmul( self.body.Q , self.body.Ib , transpose_a = True) , self.body.Q)
-        #print(Teqalpha)
-        print("teqc2=",Teqc)
         Teqc += tf.matmul( tf.cross( tf.matmul( self.body.wb , self.body.Ib ), self.body.wb) , self.body.Q)
-        print("teqc2=",Teqc)
-        print(tf.cross(tf.matmul( self.body.wb, self.body.Ib ) ,self.body.wb, name = "mac"))
-        #print("Teqc final ",Teqc);
-        #print(g)
-        #print(mlsum)
-        #print("mg = ",tf.cross(mlsum, g))
         Teqc += tf.cross(mlsum, g)
-        print("mlsum",mlsum)
-        #print("Teqc = ",Teqc);
         Teqanorm = tf.reshape(tf.matmul(mlsum, mlsum, transpose_b = True),[-1])
-        #print(mlsum)
-        #print(Teqanorm)
-        #print(tf.cross(mlsum,Feqc))
-        print("teqc=",Teqc)
         alphabs = tf.matmul( 
         Teqc - tf.scalar_mul(1./Mtot, tf.cross(mlsum,Feqc)),
         tf.matrix_inverse(
             Teqalpha - tf.scalar_mul(1./Mtot , 
             tf.diag(tf.concat([Teqanorm,Teqanorm,Teqanorm], axis=0)) - tf.matmul(mlsum,mlsum,transpose_a = True))
         ))
-        #print("alphabs = ",alphabs)
-        #print("Feqc = ",Feqc)
         asb = tf.scalar_mul(1./Mtot, Feqc - tf.cross(mlsum,alphabs))
-        #print("asb = ",asb)
-        print("alphabs", alphabs)
         self.body.wb += tf.scalar_mul(dtime, alphabs)
         self.body.Q += tf.scalar_mul(dtime,tf.cross(tf.concat([wbs, wbs,wbs], axis = 0),self.body.Q))
         self.body.vs+=tf.scalar_mul(dtime,asb)
         self.body.rs+=tf.scalar_mul(dtime,self.body.vs)
-
-        return sumDs
+        MWT=asb
+        return MWT
 R = robot()
 R.set_constants()
 return_val = R.timeflow()
-return_val = R.timeflow()
 for _ in range(1000):
     sess=tf.Session()
-    print( "Ds = ", sess.run(return_val, feed_dict={Destination:[[0,0,0]]}))
+    print( "YAY body.rs = ", sess.run(return_val, feed_dict={Destination:[[0,0,0]]}))
